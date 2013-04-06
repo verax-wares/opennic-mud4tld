86943d4ba72c4bc95f50445ff00b16d96395c88a
Martin C <martin@mchomenet.com>
2013-04-06 19:40:38 +0000

    changed license

--- init_tld/init_tld.c
+++ init_tld/init_tld.c
@@ -1,6 +1,26 @@
 /*
 	Initialise TLD
-	By Martin COLEMAN (C) 2012. All rights reserved.
+	By Martin COLEMAN (C) 2012-2013. All rights reserved.
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met: 
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer. 
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution. 
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 	Compile via gcc -o init_tld main.c -lsqlite3
 	Tiny C Compiler should work well too.
 	Licensed under the Basic Software License v1.0.
@@ -18,13 +38,16 @@
 	
 	v0.4 - 2012-06-10
 	- Full custom nameserver support is now implemented.
+	
+	v0.4a - 2013-04-06
+	- Released under BSD 2 clause license.
 */
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
 /* #include <sqlite3.h> */
 #include "sqlite3.c"
-#define VERSION "0.4"
+#define VERSION "0.4a"
 // #define VERBOSE 1
 
 sqlite3 *db;
